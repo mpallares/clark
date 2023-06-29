34da03781f31f766a39a88f644edd0d6785faae5
mpallares <mpallareslara@gmail.com>
2023-06-29 09:07:49 +0200

    Added list test

--- native/src/screens/List/List.test.tsx
+++ native/src/screens/List/List.test.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
-import { useGetProductsQuery } from '../../api/services/product';
+import { render, screen } from '@testing-library/react-native';
 import { List } from './List';
-
-jest.mock('../../api/services/product', () => ({
-  __esModule: true,
-  useGetProductsQuery: jest.fn(),
-}));
+import { ProductsList } from '../../components/ProductsList/ProductsList';
+import { NavigationContainer } from '@react-navigation/native';
 
 describe('List component', () => {
   it('renders the spinner when loading', () => {
@@ -23,51 +19,41 @@ describe('List component', () => {
 
     const { getByTestId } = render(<List />);
 
-    expect(getByTestId('spinner')).toBeDefined();
+    expect(getByTestId('activity-indicator')).toBeDefined();
   });
 
   it('renders the ProductsList component with data when not loading', () => {
     const products = [
-      { id: 1, name: 'Product 1' },
-      { id: 2, name: 'Product 2' },
+      {
+        id: 11,
+        title: 'perfume Oil',
+        description: 'Mega Discount, Impression of A...',
+        price: 13,
+        discountPercentage: 8.4,
+        rating: 4.26,
+        stock: 65,
+        brand: 'Impression of Acqua Di Gio',
+        category: 'fragrances',
+        thumbnail: 'https://i.dummyjson.com/data/products/11/thumbnail.jpg',
+        images: [
+          'https://i.dummyjson.com/data/products/11/1.jpg',
+          'https://i.dummyjson.com/data/products/11/2.jpg',
+          'https://i.dummyjson.com/data/products/11/3.jpg',
+          'https://i.dummyjson.com/data/products/11/thumbnail.jpg',
+        ],
+      },
     ];
-    const mockUseGetProductsQuery = jest.spyOn(
-      require('../../api/services/product'),
-      'useGetProductsQuery'
+
+    render(
+      <NavigationContainer>
+        <ProductsList
+          products={products}
+          refetchProducts={() => {}}
+          setEndLoading={() => {}}
+        />
+      </NavigationContainer>
     );
-    const refetchProducts = jest.fn();
 
-    mockUseGetProductsQuery.mockReturnValue({
-      data: { products },
-      isLoading: false,
-      refetch: refetchProducts,
-    });
-
-    const { getByTestId, queryByTestId } = render(<List />);
-
-    expect(queryByTestId('spinner')).toBeNull();
-    expect(getByTestId('productsList')).toBeDefined();
-    // Additional assertions for the rendered products list can be added here.
+    expect(screen.getByText(products[0].title)).toBeTruthy();
   });
 });
-// import * as React from 'react';
-// import { render, screen, fireEvent, act } from '@testing-library/react-native';
-// import App from '../../../App';
-
-// describe('Testing react navigation', () => {
-//   test('page contains a button', async () => {
-//     render(<App />);
-//     const button = await screen.findByText('See Details');
-//     expect(button).toBeTruthy();
-//   });
-
-//   test('clicking on one item takes you to the details screen', async () => {
-//     render(<App />);
-//     await act(async () => {
-//       const toPress = screen.getByText('See Details');
-//       fireEvent(toPress, 'press');
-//     });
-//     const details = screen.findByText('Details');
-//     expect(details).toBeTruthy();
-//   });
-// });
